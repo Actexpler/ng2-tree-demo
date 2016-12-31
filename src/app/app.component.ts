67a05f046c713fbcfe19854a35e09ee735d5badd
geo <rychko.georgiy@gmail.com>
2016-12-31 19:22:10 +0200

    feat(tree): add ng2-tree as a dependency, render simple tree using angular-cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,163 @@
 import { Component } from '@angular/core';
+import { NodeEvent, TreeModel, RenamableNode } from 'ng2-tree';
+
+declare let alertify: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="tree-demo-app">
+      <div class="tree-container">
+        <p>Fonts tree</p>
+        <tree
+          [tree]="fonts" 
+          (nodeRemoved)="onNodeRemoved($event)"
+          (nodeRenamed)="onNodeRenamed($event)"
+          (nodeSelected)="onNodeSelected($event)"
+          (nodeMoved)="onNodeMoved($event)"
+          (nodeCreated)="onNodeCreated($event)">
+        </tree>
+      </div>
+      <div class="tree-container">
+        <p>Programming languages tree</p>
+        <tree 
+          [tree]="pls" 
+          (nodeRemoved)="onNodeRemoved($event)"
+          (nodeRenamed)="onNodeRenamed($event)"
+          (nodeSelected)="onNodeSelected($event)"
+          (nodeMoved)="onNodeMoved($event)"
+          (nodeCreated)="onNodeCreated($event)">
+        </tree>
+      </div>
+    </div>
+    `,
+  styles: [`
+   .tree-demo-app {
+      margin: auto;
+      width: -moz-fit-content;
+      width: -webkit-fit-content;
+      width: fit-content;
+    }
+    .tree-demo-app .tree-container {
+      float: left;
+      vertical-align: top;
+      width: 500px;
+    }
+    .tree-demo-app .tree-container p {
+      color: #40a070;
+      font-size: 2em;
+    } 
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  public fonts: TreeModel = {
+    value: 'Fonts',
+    children: [
+      {
+        value: 'Serif',
+        children: [
+          {value: 'Antiqua'},
+          {value: 'DejaVu Serif'},
+          {value: 'Garamond'},
+          {value: 'Georgia'},
+          {value: 'Times New Roman'},
+          {
+            value: 'Slab serif',
+            children: [
+              {value: 'Candida'},
+              {value: 'Swift'},
+              {value: 'Guardian Egyptian'}
+            ]
+          }
+        ]
+      },
+      {
+        value: 'Sans-serif',
+        children: [
+          {value: 'Arial'},
+          {value: 'Century Gothic'},
+          {value: 'DejaVu Sans'},
+          {value: 'Futura'},
+          {value: 'Geneva'},
+          {value: 'Liberation Sans'}
+        ]
+      },
+      {
+        value: 'Monospace',
+        children: [
+          {value: 'Input Mono'},
+          {value: 'Roboto Mono'},
+          {value: 'Liberation Mono'},
+          {value: 'Hack'},
+          {value: 'Consolas'},
+          {value: 'Menlo'},
+          {value: 'Source Code Pro'}
+        ]
+      }
+    ]
+  };
+
+  public pls: TreeModel = {
+    value: 'Programming languages by programming paradigm',
+    children: [
+      {
+        value: 'Aspect-oriented programming',
+        children: [
+          {value: 'AspectJ'},
+          {value: 'AspectC++'}
+        ]
+      },
+      {
+        value: 'Object-oriented programming',
+        children: [
+          {
+            value: {
+              name: 'Java',
+              setName(name: string): void {
+                this.name = name;
+              },
+              toString(): string {
+                return this.name;
+              }
+            } as RenamableNode
+          },
+          {value: 'C++'},
+          {value: 'C#'}
+        ]
+      },
+      {
+        value: 'Prototype-based programming',
+        children: [
+          {value: 'JavaScript'},
+          {value: 'CoffeeScript'},
+          {value: 'TypeScript'}
+        ]
+      }
+    ]
+  };
+
+  public onNodeRemoved(e: NodeEvent): void {
+    this.logEvent(e, 'Removed');
+  }
+
+  public onNodeMoved(e: NodeEvent): void {
+    this.logEvent(e, 'Moved');
+  }
+
+  public onNodeRenamed(e: NodeEvent): void {
+    this.logEvent(e, 'Renamed');
+  }
+
+  public onNodeCreated(e: NodeEvent): void {
+    this.logEvent(e, 'Created');
+  }
+
+  public onNodeSelected(e: NodeEvent): void {
+    this.logEvent(e, 'Selected');
+  }
+
+  public logEvent(e: NodeEvent, message: string): void {
+    console.log(e);
+    alertify.message(`${message}: ${e.node.value}`);
+  }
 }
