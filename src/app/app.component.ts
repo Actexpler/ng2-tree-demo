33708481d66c7680756b8c8b17027337fa7ac6eb
Georgiy Rychko <rychko.georgiy@gmail.com>
2017-03-04 16:04:12 +0200

    chore(*): update deps and tree examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { NodeEvent, TreeModel, RenamableNode } from 'ng2-tree';
+import { Component, OnInit } from '@angular/core';
+import { NodeEvent, TreeModel, RenamableNode, Ng2TreeSettings } from 'ng2-tree';
 
-declare let alertify: any;
+declare const alertify: any;
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   template: `
     <div class="tree-demo-app">
       <div class="tree-container">
-        <p>Fonts tree</p>
+        <p class="tree-title">Fonts tree</p>
         <tree
           [tree]="fonts"
           (nodeRemoved)="onNodeRemoved($event)"
@@ -19,9 +19,11 @@ declare let alertify: any;
         </tree>
       </div>
       <div class="tree-container">
-        <p>Programming languages tree</p>
+        <p class="tree-title">Programming languages tree</p>
+        <p class="notice">this tree is loaded asynchronously</p>
         <tree
           [tree]="pls"
+          [settings]="settings"
           (nodeRemoved)="onNodeRemoved($event)"
           (nodeRenamed)="onNodeRenamed($event)"
           (nodeSelected)="onNodeSelected($event)"
@@ -43,33 +45,42 @@ declare let alertify: any;
       vertical-align: top;
       width: 500px;
     }
-    .tree-demo-app .tree-container p {
+    .tree-title {
       color: #40a070;
       font-size: 2em;
     }
+    .notice {
+      color: #e91e63;
+      font-size: 1.2em;
+      font-style: italic;
+    }
   `]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public settings: Ng2TreeSettings = {
+    rootIsVisible: false
+  };
+
   public fonts: TreeModel = {
     value: 'Fonts',
     children: [
       {
         value: 'Serif  -  All my children and I are STATIC ¯\\_(ツ)_/¯',
         settings: {
-          static: true
+          'static': true
         },
         children: [
-          {value: 'Antiqua'},
-          {value: 'DejaVu Serif'},
-          {value: 'Garamond'},
-          {value: 'Georgia'},
-          {value: 'Times New Roman'},
+          { value: 'Antiqua' },
+          { value: 'DejaVu Serif' },
+          { value: 'Garamond' },
+          { value: 'Georgia' },
+          { value: 'Times New Roman' },
           {
             value: 'Slab serif',
             children: [
-              {value: 'Candida'},
-              {value: 'Swift'},
-              {value: 'Guardian Egyptian'}
+              { value: 'Candida' },
+              { value: 'Swift' },
+              { value: 'Guardian Egyptian' }
             ]
           }
         ]
@@ -77,89 +88,101 @@ export class AppComponent {
       {
         value: 'Sans-serif',
         children: [
-          {value: 'Arial'},
-          {value: 'Century Gothic'},
-          {value: 'DejaVu Sans'},
-          {value: 'Futura'},
-          {value: 'Geneva'},
-          {value: 'Liberation Sans'}
+          { value: 'Arial' },
+          { value: 'Century Gothic' },
+          { value: 'DejaVu Sans' },
+          { value: 'Futura' },
+          { value: 'Geneva' },
+          { value: 'Liberation Sans' }
         ]
       },
       {
-        value: 'Monospace',
-        children: [
-          {value: 'Input Mono'},
-          {value: 'Roboto Mono'},
-          {value: 'Liberation Mono'},
-          {value: 'Hack'},
-          {value: 'Consolas'},
-          {value: 'Menlo'},
-          {value: 'Source Code Pro'}
-        ]
+        value: 'Monospace - With ASYNC CHILDREN',
+        // children property is ignored if "loadChildren" is present
+        children: [{value: 'I am the font that will be ignored'}],
+        loadChildren: (callback) => {
+          setTimeout(() => {
+            callback([
+              { value: 'Input Mono' },
+              { value: 'Roboto Mono' },
+              { value: 'Liberation Mono' },
+              { value: 'Hack' },
+              { value: 'Consolas' },
+              { value: 'Menlo' },
+              { value: 'Source Code Pro' }
+            ]);
+          }, 5000);
+        }
       }
     ]
   };
 
-  public pls: TreeModel = {
-    value: 'Programming languages by programming paradigm',
-    children: [
-      {
-        value: 'Aspect-oriented programming',
-        children: [
-          {value: 'AspectJ'},
-          {value: 'AspectC++'}
-        ]
-      },
-      {
-        value: 'Object-oriented programming',
+  public pls: TreeModel;
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.pls = {
+        value: 'Programming languages by programming paradigm',
         children: [
           {
-            value: {
-              name: 'Java',
-              setName(name: string): void {
-                this.name = name;
-              },
-              toString(): string {
-                return this.name;
-              }
-            } as RenamableNode
+            value: 'Aspect-oriented programming',
+            children: [
+              { value: 'AspectJ' },
+              { value: 'AspectC++' }
+            ]
           },
-          {value: 'C++'},
-          {value: 'C#'}
+          {
+            value: 'Object-oriented programming',
+            children: [
+              {
+                value: {
+                  name: 'Java',
+                  setName(name: string): void {
+                    this.name = name;
+                  },
+                  toString(): string {
+                    return this.name;
+                  }
+                } as RenamableNode
+              },
+              { value: 'C++' },
+              { value: 'C#' }
+            ]
+          },
+          {
+            value: 'Prototype-based programming',
+            children: [
+              { value: 'JavaScript' },
+              { value: 'CoffeeScript' },
+              { value: 'TypeScript' }
+            ]
+          }
         ]
-      },
-      {
-        value: 'Prototype-based programming',
-        children: [
-          {value: 'JavaScript'},
-          {value: 'CoffeeScript'},
-          {value: 'TypeScript'}
-        ]
-      }
-    ]
-  };
+      };
+    }, 2000);
+  }
 
   public onNodeRemoved(e: NodeEvent): void {
-    this.logEvent(e, 'Removed');
+    AppComponent.logEvent(e, 'Removed');
   }
 
   public onNodeMoved(e: NodeEvent): void {
-    this.logEvent(e, 'Moved');
+    AppComponent.logEvent(e, 'Moved');
   }
 
   public onNodeRenamed(e: NodeEvent): void {
-    this.logEvent(e, 'Renamed');
+    AppComponent.logEvent(e, 'Renamed');
   }
 
   public onNodeCreated(e: NodeEvent): void {
-    this.logEvent(e, 'Created');
+    AppComponent.logEvent(e, 'Created');
   }
 
   public onNodeSelected(e: NodeEvent): void {
-    this.logEvent(e, 'Selected');
+    AppComponent.logEvent(e, 'Selected');
   }
 
-  public logEvent(e: NodeEvent, message: string): void {
+  private static logEvent(e: NodeEvent, message: string): void {
     console.log(e);
     alertify.message(`${message}: ${e.node.value}`);
   }
